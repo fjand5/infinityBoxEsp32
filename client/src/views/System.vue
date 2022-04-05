a70c99a67f53db9424addbcfbcc3aba1be395ad0
fjand5 <huynhtheluat@gmail.com>
2022-04-05 20:45:24 +0700

    cm

--- client/src/views/System.vue
+++ client/src/views/System.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="system">
-      day la system
+    <el-row :gutter="5">
+      <el-col :span="24">
+        <el-input placeholder="Router Wifi" v-model="ssid">
+          <template slot="prepend">WIFI</template>
+          <template slot="append">
+             <el-button>
+               OK
+             </el-button>
+          </template>
+        </el-input>
+      </el-col>
+      <el-col :span="24">
+        <el-input placeholder="Router Wifi" v-model="ssid">
+          <template slot="prepend">WIFI</template>
+          <template slot="append">
+             <el-button>
+               OK
+             </el-button>
+          </template>
+        </el-input>
+      </el-col>
+    </el-row>
+   
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
